573ed71ea101851cc50822b842ed1b3110c298fb
James Mitchell <james.mitchell@maungawhau.net.nz>
2020-09-07 11:37:03 +1200

    #4 add similarity check to form validation

--- test-project/testapp/forms.py
+++ test-project/testapp/forms.py
@@ -3,7 +3,7 @@ from django.core.exceptions import ValidationError
 
 from .models import TextBlob
 from .constants import MIN_WORD_COUNT, MAX_WORD_COUNT
-from .tools import splitWords
+from .tools import splitWords, isSimilar
 
 # form and validation for text input
 class TextBlobForm(ModelForm):
@@ -18,3 +18,8 @@ class TextBlobForm(ModelForm):
             raise ValidationError('Not enough words. We need at least {count} words.'.format(count=MIN_WORD_COUNT), 'word_count')
         if len(words) > MAX_WORD_COUNT:
             raise ValidationError('Too many words. We need no more than {count} words.'.format(count=MAX_WORD_COUNT), 'word_count')
+
+        # reject input that is too similar to our stored messages
+        old_messages = TextBlob.objects.values_list('text', flat=True)
+        if isSimilar(self.cleaned_data['text'], old_messages):
+            raise ValidationError('Too similar to the existing messages', 'similar')
